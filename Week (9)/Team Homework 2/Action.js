f93fd388904cd6e3596f34ab1b0e9637192038e0
Lee Van Hiep <daihiep2k@gmail.com>
2019-12-17 11:54:35 +0700

    Bài tập lớn

---
+++ Week (9)/Team Homework 2/Action.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+	window.onscroll = function() {Stick()};
+
+	var navbar = document.getElementById("Menu");
+	var top = navbar.offsetTop;
+
+	function Stick()
+	{
+	  if(window.pageYOffset >= top)
+	  	navbar.classList.add("Sticky");
+	  else
+	    navbar.classList.remove("Sticky");
+	}
+});
+
+window.onload = function()
+{
+	var Index = 0;
+	var x = document.getElementsByClassName("Slide");
+	NextSlide();
+	function NextSlide()
+	{
+		if(Index==0) x[3].style.display = "none";
+		else x[Index-1].style.display = "none";
+		if(Index==x.length) Index = 0;
+		x[Index].style.display = "flex";
+		Index++;
+		/*setTimeout(NextSlide,6000);*/
+	}
+}
